841e42b681b4908ca0316edcfec36a30abcfdcae
Adil-Iqbal <main@adil-iqbal.com>
2022-12-20 14:54:30 -0600

    data layer start

--- iqbacli/data/sql/init_query_table.sql
+++ iqbacli/data/sql/init_query_table.sql
@@ -1,6 +1,16 @@
-DROP TABLE query IF EXISTS;
-
-CREATE TABLE query {
-    id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS query {
+    id INT UNIQUE NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+    keywords_raw VARCHAR(500) NOT NULL,
+    keywords_pattern VARCHAR(1500) NOT NULL,
+    directory VARCHAR(500) NOT NULL,
+    output_dir VARCHAR(500),
+    cache BOOLEAN NOT NULL,
+    flat BOOLEAN NOT NULL,
+    regex BOOLEAN NOT NULL, 
+    only_ext VARCHAR(500),
+    only_filename VARCHAR(500),
+    only_dirname VARCHAR(500),
+    ignore_ext VARCHAR(500),
+    ignore_filename VARCHAR(500),
+    ignore_dirname VARCHAR(500),
 }
