4428ced4050ffe65814b45f79e604bc8c2c20472
Adil-Iqbal <main@adil-iqbal.com>
2023-01-04 20:55:05 -0600

    more logging

--- src/iqbacli/logging.py
+++ src/iqbacli/logging.py
@@ -9,19 +9,26 @@ dotenv.load_dotenv()
 BASE_DIR = Path(__file__).parent.resolve()
 
 
-def create_logger(name: str) -> Logger:
+def get_relative_path(filepathstr: str) -> str:
+    filepath: Path = Path(filepathstr)
+    relpath = filepath.relative_to(BASE_DIR)
+    return str(relpath)
+
+
+def create_logger(filepath: str) -> Logger:
+    relpath = get_relative_path(filepath)
     if os.getenv("IQBA_ENV") == "dev":
-        dev_logger = logging.getLogger(name)
+        dev_logger = logging.getLogger(relpath)
         dev_logger.setLevel(logging.INFO)
         dev_handler = StreamHandler()
         dev_formatter = Formatter(
-            fmt="%(levelname)s | %(filename)s:%(lineno)d | %(asctime)s | %(message)s"
+            fmt="%(levelname)s | %(name)s:%(lineno)d | %(asctime)s | %(message)s"
         )
         dev_handler.setFormatter(dev_formatter)
         dev_logger.addHandler(dev_handler)
         return dev_logger
 
     prod_handler = NullHandler()
-    prod_logger = logging.getLogger(name)
+    prod_logger = logging.getLogger(relpath)
     prod_logger.addHandler(prod_handler)
     return prod_logger
