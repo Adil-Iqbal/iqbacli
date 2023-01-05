23460971412927e1113263cbfd08d4dd88989466
Adil-Iqbal <main@adil-iqbal.com>
2023-01-05 01:57:37 -0600

    refactor logger, add envvar for logging

--- src/iqbacli/logging.py
+++ src/iqbacli/logging.py
@@ -1,34 +1,77 @@
+import functools
 import os
+from typing import Final
+import datetime
 import dotenv
 import logging
 from pathlib import Path
-from logging import Logger, NullHandler, StreamHandler, Formatter
+from logging import FileHandler, Logger, NullHandler, StreamHandler, Formatter
 
 dotenv.load_dotenv()
 
-BASE_DIR = Path(__file__).parent.resolve()
+BASE_DIR: Final[Path] = Path(__file__).parent.resolve()
+SRC_DIR: Final[Path] = BASE_DIR.parent.parent.resolve()
+LOG_DIR: Final[Path] = SRC_DIR / "logs"
+_DEFAULT_LOG_FORMATTER: Final[Formatter] = Formatter(
+    fmt="%(asctime)s.%(msecs)03d | %(levelname)s | %(name)s:%(lineno)d | %(message)s",
+    datefmt="%Y-%m-%d,%H:%M:%S",
+)
 
 
-def get_relative_path(filepathstr: str) -> str:
+def _get_relative_path(filepathstr: str) -> str:
     filepath: Path = Path(filepathstr)
     relpath = filepath.relative_to(BASE_DIR)
     return str(relpath)
 
 
-def create_logger(filepath: str) -> Logger:
-    relpath = get_relative_path(filepath)
-    if os.getenv("IQBA_ENV") == "dev":
-        dev_logger = logging.getLogger(relpath)
-        dev_logger.setLevel(logging.INFO)
-        dev_handler = StreamHandler()
-        dev_formatter = Formatter(
-            fmt="%(asctime)s | %(levelname)s | %(name)s:%(lineno)d | %(message)s",
-        )
-        dev_handler.setFormatter(dev_formatter)
-        dev_logger.addHandler(dev_handler)
-        return dev_logger
+def _create_log_dir_if_needed() -> None:
+    if LOG_DIR.exists() and LOG_DIR.is_dir():
+        return
+    LOG_DIR.mkdir()
 
+
+def _resolve_log_level():
+    if (env := os.getenv("IQBA_LOG_LEVEL")) is not None:
+        return logging._nameToLevel[env.upper()]
+    return logging.INFO
+
+
+@functools.cache
+def _create_log_file_path() -> str:
+    now: str = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")[:-3]
+    filepath: Path = LOG_DIR / f"iqba_{now}.log"
+    return str(filepath.absolute())
+
+
+def _resolve_log_streaming(logger: Logger) -> None:
+    if os.getenv("IQBA_STREAM_LOGS") == "1":
+        stream_handler = StreamHandler()
+        stream_handler.setFormatter(_DEFAULT_LOG_FORMATTER)
+        logger.addHandler(stream_handler)
+
+
+def _create_dev_logger(relpath: str) -> Logger:
+    dev_logger = logging.getLogger(relpath)
+    _create_log_dir_if_needed()
+
+    dev_logger.setLevel(_resolve_log_level())
+    dev_file_handler = FileHandler(_create_log_file_path())
+    dev_file_handler.setFormatter(_DEFAULT_LOG_FORMATTER)
+    dev_logger.addHandler(dev_file_handler)
+    _resolve_log_streaming(dev_logger)
+
+    return dev_logger
+
+
+def _create_prod_logger(relpath: str) -> Logger:
     prod_handler = NullHandler()
     prod_logger = logging.getLogger(relpath)
     prod_logger.addHandler(prod_handler)
     return prod_logger
+
+
+def create_logger(filepath: str) -> Logger:
+    relpath = _get_relative_path(filepath)
+    if os.getenv("IQBA_ENV") == "dev":
+        return _create_dev_logger(relpath)
+    return _create_prod_logger(relpath)
