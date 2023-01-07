a4270a4254c43d0fbb34764cdba32336d8a79a70
Adil-Iqbal <main@adil-iqbal.com>
2023-01-07 00:06:17 -0600

    more config tests

--- tests/data/test_config.py
+++ tests/data/test_config.py
@@ -1,35 +1,50 @@
+import pytest
 from pathlib import Path
 from iqbacli.data.config import Config
 from iqbacli.params import builtins
 
 
-def test_create_new_config():
-    test_config_path = Path(__file__).parent.resolve() / "config.json"
+@pytest.fixture
+def config_path():
+    return Path(__file__).parent.resolve() / "config.json"
+
+
+@pytest.fixture
+def sample_config(config_path):
     try:
-        assert not test_config_path.exists()
-        config = Config.create_new_config(test_config_path)
-        assert test_config_path.exists()
+        yield Config.create_new_config(config_path)
+    finally:
+        config_path.unlink(missing_ok=True)
+
+
+def test_create_new_config(config_path):
+    try:
+        assert not config_path.exists()
+        config = Config.create_new_config(config_path)
+        assert config_path.exists()
         assert config.cache == builtins.CACHE
         assert config.flat == builtins.FLAT
         assert config.regex == builtins.REGEX
     finally:
-        test_config_path.unlink(missing_ok=True)
+        config_path.unlink(missing_ok=True)
 
 
-def test_config_to_dict():
-    test_config_path = Path(__file__).parent.resolve() / "config.json"
-    try:
-        config_dict = Config.create_new_config(test_config_path)._to_dict()
-        assert config_dict["cache"] == builtins.CACHE
-        assert config_dict["flat"] == builtins.FLAT
-        assert config_dict["regex"] == builtins.REGEX
-    finally:
-        test_config_path.unlink(missing_ok=True)
+def test_config_to_dict(sample_config: Config):
+    config_dict = sample_config._to_dict()
+    assert config_dict["cache"] == builtins.CACHE
+    assert config_dict["flat"] == builtins.FLAT
+    assert config_dict["regex"] == builtins.REGEX
 
 
-def test_config_save():
-    ...
+def test_config_save(sample_config: Config, config_path: Path):
+    sample_config.ignore_ext = "json"
+    sample_config.save()
+    reload_config = Config.get_config(config_path)
+    assert reload_config.ignore_ext == "json"
 
 
-def test_config_get():
-    ...
+def test_config_get(sample_config: Config, config_path: Path):
+    reload_config = Config.get_config(config_path)
+    assert sample_config.cache == reload_config.cache
+    assert sample_config.flat == reload_config.flat
+    assert sample_config.regex == reload_config.regex
