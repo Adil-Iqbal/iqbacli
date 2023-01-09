0642e1d2c30ab6e0a81da27f5c4a468890f27c72
Adil-Iqbal <main@adil-iqbal.com>
2023-01-09 13:26:45 -0600

    clean up paths file

--- src/iqbacli/paths.py
+++ src/iqbacli/paths.py
@@ -12,17 +12,10 @@ SQL_DIR = BASE_DIR / "data" / "sql"
 # User Paths
 APP_DIRS: Final[Any] = PlatformDirs(appname="iqbacli")
 USER_DATA_DIR: Final[Path] = Path(APP_DIRS.user_data_dir)
-
-
-def _resolve_log_dir() -> Path:
-    if "IQBA_ENV" in os.environ and os.getenv("IQBA_ENV") == "dev":
-        return ROOT_DIR / "logs"
-    return USER_DATA_DIR / "logs"
-
-
 CONFIG_PATH: Final[Path] = Path(APP_DIRS.user_config_dir) / "iqbaconfig.json"
 DB_PATH: Final[Path] = USER_DATA_DIR / "database.sqlite3"
-LOG_DIR: Final[Path] = _resolve_log_dir()
+_log_paths: dict[str, Path] = {"dev": ROOT_DIR, "prod": USER_DATA_DIR}
+LOG_DIR: Final[Path] = _log_paths[os.getenv("IQBA_ENV", "prod")] / "logs"
 
 
 # Log all paths in file.
