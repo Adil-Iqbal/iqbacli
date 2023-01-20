d656d02afa10e2b265e826f04f4d064dfa15135d
Adil-Iqbal <main@adil-iqbal.com>
2023-01-19 23:46:30 -0600

    iqba config get logic

--- src/iqbacli/cli/config/get.py
+++ src/iqbacli/cli/config/get.py
@@ -1,18 +1,57 @@
+from typing import NoReturn, Optional
 import humps
 import typer
+from ...driver import config
+
+from iqbacli.cli.printer import printer_factory
 
 app = typer.Typer(short_help="Get configuration settings.")
 
 
+def _print_config_only(json: bool, no_color: bool) -> None:
+    printer = printer_factory(json, no_color)
+    printer.print_config()
+
+
 @app.callback(invoke_without_command=True)
 def get(
-    param: str = typer.Option(
-        default=None, show_default=False, help="Get a specific parameter."
-    )
+    key: Optional[str] = typer.Option(
+        default=None,
+        show_default=False,
+        help="Get value for a specific configuration key.",
+    ),
+    json: bool = typer.Option(
+        False,
+        "--json/",
+        show_default=False,
+        help="Output json.",
+        rich_help_panel="Formatting",
+    ),
+    no_color: bool = typer.Option(
+        False,
+        "--no-color/",
+        show_default=False,
+        help="Remove color.",
+        rich_help_panel="Formatting",
+    ),
 ):
     """
     Get entire configuration. / Get a specific parameter.\n
-    (entire config) iqba config get\n
-    (just a param)  iqba config get --param flat\n
+    (entire config)  iqba config get\n
+    (config as json) iqba config get --json\n
+    (just a param)   iqba config get --key flat\n
     """
-    print(f"get (with param?) {param}")
+    if key is None:
+        _print_config_only(json, no_color)
+        raise typer.Exit()
+
+    if humps.is_kebabcase(key):
+        key = humps.dekebabize(key)
+
+    config_dict = config.get_config_dict()
+    if key not in config_dict:
+        _print_config_only(json, no_color)
+        typer.secho(f"\nUnknown key; {key}", fg="red")
+        raise typer.Abort()
+
+    typer.echo(config_dict[key])
